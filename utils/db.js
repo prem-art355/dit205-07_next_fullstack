487774b32a6ed5ba63f1ca6ab8d19ad58868a4f9
Prem Art355 <premhajung@gmail.com>
2025-11-11 01:34:58 +0700

    db

--- utils/db.js
+++ utils/db.js
@@ -1,14 +1,17 @@
 import mysql from "mysql2/promise";
 
 export const mysqlPool = mysql.createPool({
-  host: process.env.MYSQL_HOST || "localhost",
-  user: process.env.MYSQL_USER || "root",
-  password: process.env.MYSQL_PASSWORD || "",
-  database: process.env.MYSQL_DATABASE || "6705525_dit205",
-  port: Number(process.env.MYSQL_PORT) || 3306,
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASS || "",
+  database: process.env.DB_NAME || "u6705525_dit205",
+  port: Number(process.env.DB_PORT || 3306),
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0,
+  ssl:
+    process.env.DB_SSL === "true"
+      ? { minVersion: "TLSv1.2", rejectUnauthorized: true }
+      : undefined,
 });
 
 // mysql2/promise already returns a promise pool suitable for async/await
